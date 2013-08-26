3832f4f0fce1d8b457fc4bb51da13967d3d0f673
Brent Boghosian <brent.boghosian@remote-learner.net>
2013-08-26 14:14:05 -0400

    ELIS 2.4.3.0 release

--- core/repository/elis_files/tests/constants.php
+++ core/repository/elis_files/tests/constants.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * ELIS(TM): Enterprise Learning Intelligence Suite
- * Copyright (C) 2008-2011 Remote-Learner.net Inc (http://www.remote-learner.net)
+ * Copyright (C) 2008-2013 Remote-Learner.net Inc (http://www.remote-learner.net)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,13 +16,20 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * @package    blocks
- * @subpackage repository
+ * @package    repository_elis_files
  * @author     Remote-Learner.net Inc
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
- * @copyright  (C) 2008-2011 Remote Learner.net Inc http://www.remote-learner.net
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @copyright  (C) 2008-2013 Remote Learner.net Inc http://www.remote-learner.net
  *
  */
 
-$plugin->version = 2011110300;
-$plugin->release = '2.4.3.0 (Build: 20130821)';
+define('ONE_MB_BYTES', 1048576);
+define('ELIS_FILES_PREFIX', 'elis_files_test_file_upload_');
+define('FILE_NAME_PREFIX', 'elis_files_test_file_type_upload');
+define('FOLDER_NAME_PREFIX', 'elis_files_test_folder_');
+define('TEST_PREFIX', 'elis_files_test_');
+define('CHAR_POS_L', 'start');
+define('CHAR_POS_M', 'middle');
+define('CHAR_POS_R', 'end');
+define('UTILITY_METHODS_PREFIX', 'elis_files_test_utility_methods');
+define('USERS_HOME', 'userhomedir');
